1f7684c042e2eaee1fbacedd724cdad361dd4917
gabriel <gabrielvinces2@gmail.com>
2026-05-05 21:04:24 -0300

    Corrigindo a data

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 AOS.init();
+
+const dataDoEvento = new Date("Dec 12, 2048 19:00:00");
+const timeStempDoEvento = dataDoEvento.getTime();
+
+const contaHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+    const distanciaEvento = timeStempDoEvento - timeStampAtual;
+
+    const diasEvento = Math.floor(distanciaEvento/ (1000 * 60 * 60 * 24));
+    const horasEvento = Math.floor((distanciaEvento % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutosEvento = Math.floor((distanciaEvento % (1000 * 60 * 60)) / (1000 * 60));
+    const segEvento = Math.floor((distanciaEvento % (1000*60)) / 1000);
+
+    document.getElementById('contador').innerHTML = `Dias - [${diasEvento}] Horas - [${horasEvento}] minutos - [${minutosEvento}] segundos - [${segEvento}]`;
+}, 1000);
